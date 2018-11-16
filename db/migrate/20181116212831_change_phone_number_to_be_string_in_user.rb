a12ff828af7291b36e09a81e4e8893ac326ac83b
ritchey0713 <david@osa.io>
2018-11-16 16:29:31 -0500

    fix data type

---
+++ db/migrate/20181116212831_change_phone_number_to_be_string_in_user.rb
@@ -0,0 +1,5 @@
+class ChangePhoneNumberToBeStringInUser < ActiveRecord::Migration[5.2]
+  def change
+    change_column :users, :phone_number, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_16_211046) do
+ActiveRecord::Schema.define(version: 2018_11_16_212831) do
 
   create_table "audits", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "auditable_id"
