51f7a364865705cff31522bd76d86bf29199415c
ritchey0713 <david@osa.io>
2018-11-16 16:25:46 -0500

    fix routing

---
+++ config/initializers/phonelib.rb
@@ -0,0 +1,5 @@
+Phonelib.default_country = "US"
+
+Phonelib.parse_special = true
+
+Phonelib.strict_check = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :posts
+  root to: "home#landing_page"
+
+  get '/users/:id' => 'users#show'
+  get '/users' => 'users#index'
+
+  get '/time_fiddler' => 'home#time_fiddler'
+  post '/time_fiddler' => 'home#time_fiddler'
 end
